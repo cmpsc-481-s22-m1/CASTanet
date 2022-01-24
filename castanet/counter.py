9905351ecd936e2dc2358b3a7ed2bc072459beb1
hinckleyc <46756003+hinckleyc@users.noreply.github.com>
2022-01-24 14:02:41 -0500

    Working on making it output for/while loops

--- castanet/counter.py
+++ castanet/counter.py
@@ -18,18 +18,31 @@ def match_imports(cast_dict):
         # Find CASTs for each of these files
         cast = cast_dict[file]
         # Determine number of import statements for each file
-        imports = match.findall(cast, match.Import())
+        imports = match.findall(cast, match.For.Iter())
         imports_dictionary[file] = len(imports)
 
     return imports_dictionary
 
 
+def match_import(cast_dict):
+    """A function for counting the number of if statements in a Python program."""
+    imports_dictionary = {}
+    # Iterate through all of the Python files in a directory
+    for file in cast_dict:
+        # Find CASTs for each of these files
+        cast = cast_dict[file]
+        # Determine number of import statements for each file
+        imports = match.findall(cast, match.While.body())
+        imports_dictionary[file] = len(imports)
+
+    return imports_dictionary
+
 if __name__ == "__main__":
-    directory = "/home/mkapfhammer/Documents/Allegheny/2022/Spring/CMPSC481/project-team-1/hello"
+    directory = "say_hello.py"
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
 
-    imports_dictionary = match_imports(tree_dict)
+    imports_dictionary = match_imports(tree_dict), match_import(tree_dict)
     print(imports_dictionary)
 
