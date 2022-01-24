178e9e148445eea59e8a1516a04cfd0004214108
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-24 13:55:01 -0500

    Removed Import statment functionality

--- castanet/counter.py
+++ castanet/counter.py
@@ -3,24 +3,7 @@ from typing import List, Tuple, Dict, Optional
 # Import LibCST
 import libcst as cst
 import generate_trees as generator
-
 import libcst.matchers as match
-    
-# TODO: Create function definitions related to all necessary user stories (should look somewhat like match_imports)
-# TODO: Modify annotations to have node key as type and key as necessary information
-
-def match_imports(cast_dict):
-    """A function for counting the number of if statements in a Python program."""
-    imports_dictionary = {}
-    # Iterate through all of the Python files in a directory
-    for file in cast_dict:
-        # Find CASTs for each of these files
-        cast = cast_dict[file]
-        # Determine number of import statements for each file
-        imports = match.findall(cast, match.Import())
-        imports_dictionary[file] = len(imports)
-
-    return imports_dictionary
 
 def match_ifstatements(cast_dict):
     """A function for counting the number of if statements in a Python program."""
@@ -34,9 +17,6 @@ def match_ifstatements(cast_dict):
         ifstatements_dictionary[file] = len(ifs)
 
     return ifstatements_dictionary
-
-    
-
 
 if __name__ == "__main__":
     directory = "/Users/nolanthompson/Desktop/College/CS481/CASTanet/hello"
