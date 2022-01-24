dd8395c0aa1ae8c25e43ee0d9675322e14a4518b
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2022-01-24 09:57:19 -0500

    Add import statement and new function for testing.

--- hello/say_hello.py
+++ hello/say_hello.py
@@ -1,10 +1,13 @@
 """This module implements the ability to greet users by name."""
+import os
 
 def greet(name):
     """Greet a user by name."""
     greeting = f"Hello, {name}!"
     print(greeting)
 
+def new_function_for_tests(nothing):
+    print(nothing)
 
 if __name__ == "__main__":
     greet("World")
