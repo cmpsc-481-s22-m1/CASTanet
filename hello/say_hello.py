8eff96da8929ae706ae60e418c3c49b39ca9b1f3
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2022-01-24 10:04:48 -0500

    Correct linting issues in say_hello.py.

--- hello/say_hello.py
+++ hello/say_hello.py
@@ -1,13 +1,16 @@
 """This module implements the ability to greet users by name."""
-import os
+import math
 
 def greet(name):
     """Greet a user by name."""
     greeting = f"Hello, {name}!"
     print(greeting)
 
-def new_function_for_tests(nothing):
-    print(nothing)
+def new_function_for_tests(number):
+    """A new function to determine the square root of a number."""
+    square_root = math.sqrt(number)
+    print(square_root)
+
 
 if __name__ == "__main__":
     greet("World")
