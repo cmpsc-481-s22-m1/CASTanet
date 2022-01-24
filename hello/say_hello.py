ce62b93726fbeb68fb6cb972a566f23ad112d79a
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2022-01-24 10:08:08 -0500

    Removee changes in say_hello.py.

--- hello/say_hello.py
+++ hello/say_hello.py
@@ -1,15 +1,9 @@
 """This module implements the ability to greet users by name."""
-import math
 
 def greet(name):
     """Greet a user by name."""
     greeting = f"Hello, {name}!"
     print(greeting)
-
-def new_function_for_tests(number):
-    """A new function to determine the square root of a number."""
-    square_root = math.sqrt(number)
-    print(square_root)
 
 
 if __name__ == "__main__":
