30deca14415e008c82015821f38d682abb1188db
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-25 09:44:40 -0500

    Started Test Cases

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+"""This module test the counter.py module regarding if statements"""
+import pytest
+from hello import counter
+
+@pytest.mark.parametrize(
+    "string, expected"
+    [
+        ("", )
+    ]
+)
+
+def test_identifing_ifs():
+    """Check that match_ifstatements identifies all of the if-statements in a directory"""
