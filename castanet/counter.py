2dc2e46cac30a13577a374d07194e0425c54942a
hinckleyc <46756003+hinckleyc@users.noreply.github.com>
2022-01-24 22:24:33 -0500

    Edited file to the user story assigned to me

--- castanet/counter.py
+++ castanet/counter.py
@@ -9,7 +9,7 @@ import libcst.matchers as match
 # TODO: Create function definitions related to all necessary user stories (should look somewhat like match_imports)
 # TODO: Modify annotations to have node key as type and key as necessary information
 
-
+"""
 def match_imports(cast_dict):
     """A function for counting the number of if statements in a Python program."""
     imports_dictionary = {}
@@ -22,16 +22,29 @@ def match_imports(cast_dict):
         imports_dictionary[file] = len(imports)
 
     return imports_dictionary
+"""
 
+def count_imports(cast_dict)"
+    """A function for countin the number of for loops in a Python program."""
+    imports_dictionary = {}
+    # Iterate through the Python files in a directory
+    for file in cast_dict:
+       # Find CASTS for every file
+       cast = cast_dict[file]
+       # Find the number of for loops in each file
+       imports = match.findall(cast, match.For.iter))
+       imports_dictionary[file] = len(imports)
+    
+    return imports_dictionary
 
-def match_import(cast_dict):
-    """A function for counting the number of if statements in a Python program."""
+def count_import(cast_dict):
+    """A function for counting the number of while loops in a Python program."""
     imports_dictionary = {}
     # Iterate through all of the Python files in a directory
     for file in cast_dict:
         # Find CASTs for each of these files
         cast = cast_dict[file]
-        # Determine number of import statements for each file
+        # Find the number of while loops each file
         imports = match.findall(cast, match.While.body())
         imports_dictionary[file] = len(imports)
 
