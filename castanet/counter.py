b0c977542a44f4b0da4912e37672a0d867ec3a32
hinckleyc <46756003+hinckleyc@users.noreply.github.com>
2022-01-25 13:51:08 -0500

    Deleted TODO statements

--- castanet/counter.py
+++ castanet/counter.py
@@ -3,27 +3,8 @@ from typing import List, Tuple, Dict, Optional
 # Import LibCST
 import libcst as cst
 import generate_trees as generator
-
 import libcst.matchers as match
     
-# TODO: Create function definitions related to all necessary user stories (should look somewhat like match_imports)
-# TODO: Modify annotations to have node key as type and key as necessary information
-
-"""
-def match_imports(cast_dict):
-    A function for counting the number of if statements in a Python program.
-    imports_dictionary = {}
-    # Iterate through all of the Python files in a directory
-    for file in cast_dict:
-        # Find CASTs for each of these files
-        cast = cast_dict[file]
-        # Determine number of import statements for each file
-        imports = match.findall(cast, match.For.iter())
-        imports_dictionary[file] = len(imports)
-
-    return imports_dictionary
-"""
-
 def count_whileloops(cast_dict):
     """A function for counting the number of while loops in a Python program."""
     imports_dictionary1 = {}
