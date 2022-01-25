afae79a7a9651f3694d975966f752bff1cc216ef
hinckleyc <46756003+hinckleyc@users.noreply.github.com>
2022-01-25 09:35:04 -0500

    Fixing errors in counter.py

--- castanet/counter.py
+++ castanet/counter.py
@@ -24,40 +24,39 @@ def match_imports(cast_dict):
     return imports_dictionary
 """
 
-def count_imports(cast_dict):
-    """A function for countin the number of for loops in a Python program."""
-    imports_dictionary = {}
-    # Iterate through the Python files in a directory
-    for file in cast_dict:
-       # Find CASTS for every file
-       cast = cast_dict[file]
-       # Find the number of for loops in each file
-       imports = match.findall(cast, match.For.iter())
-       imports_dictionary[file] = len(imports)
-    
-    return imports_dictionary
-
-def count_import(cast_dict):
+def count_whileloops(cast_dict):
     """A function for counting the number of while loops in a Python program."""
-    import_dictionary = {}
+    imports_dictionary = {}
     # Iterate through all of the Python files in a directory
     for file in cast_dict:
         # Find CASTs for each of these files
         cast = cast_dict[file]
-        # Find the number of while loops each file
-        import_ = match.findall(cast, match.While.body())
-        import_dictionary[file] = len(import_)
+        # Determine number of import statements for each file
+        imports = match.findall(cast, match.While())
+        imports_dictionary[file] = len(imports)
 
     return imports_dictionary
 
+
+def count_forloops(cast_dict):
+    """A function for coutning the number of while loops in a Python program."""
+    imports_dictionary = {}
+    # Iterate through all of the Python files in a directory
+    for file in cast_dict:
+        # Find CASTs for each of these files
+        cast = cast_dict[file]
+        # Determine number of import statements for each file
+        imports = match.findall(cast, match.While())
+        imports_dictionary[file] = len(imports)
+
+    return imports_dictionary
+
+
 if __name__ == "__main__":
-    directory = "/Users/chinckley/Desktop/Comp Sci/cs481/project-team-1/hello"
+    directory = "/home/mkapfhammer/Documents/Allegheny/2022/Spring/CMPSC481/project-team-1/hello"
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
 
-    imports_dictionary = count_imports(tree_dict)
-    import_dictionary =count_import(tree_dict)
+    imports_dictionary = match_imports(tree_dict)
     print(imports_dictionary)
-    print(import_dictionary)
-
