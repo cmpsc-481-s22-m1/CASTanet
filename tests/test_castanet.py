8ea938ab10a1703973a89b1548e8a011783bc1d2
hinckleyc <46756003+hinckleyc@users.noreply.github.com>
2022-01-25 10:28:27 -0500

    Creating a test case for castanet.py

---
+++ tests/test_castanet.py
@@ -0,0 +1,19 @@
+
+"""This module tests the castanet.counter module."""
+
+import pytest
+
+from castanet import counter
+from castanet import generate_trees as generator
+
+
+def test_countloops():
+    """Check that functions and docstrings are counted correctly"""
+    directory = "/Users/chinckley/Desktop/Comp Sci/cs481/project-team-1/hello"
+    file_list = generator.find_python_files(directory)
+    string_file_list = generator.read_files(directory, file_list)
+    tree_dict = generator.generate_cast(string_file_list)
+    imports_dictionary1 = counter.count_whileloops(tree_dict)
+    imports_dictionary = counter.count_forloops
+    assert imports_dictionary1 == {'__init__.py': 0, 'test.py': 0, 'say_hello.py': 1, 'test-while.py': 0}
+    assert imports_dictionary == {'__init__.py': 0, 'test.py': 0, 'say_hello.py': 1, 'test-while.py': 0}
