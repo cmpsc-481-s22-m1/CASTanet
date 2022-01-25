7d7a0aa9b112babf8b975844a6e7a09d6d84c725
antlet <antlet@allegheny.edu>
2022-01-24 23:03:04 -0500

    Update pyproject.toml+counter.py

--- castanet/counter.py
+++ castanet/counter.py
@@ -11,16 +11,41 @@ import libcst.matchers as match
 
 def match_imports(cast_dict):
     """A function for counting the number of import statements in a Python program."""
-    imports_dictionary = {}
+    func_count = {}
     # Iterate through all of the Python files in a directory
     for file in cast_dict:
+        # track the number of docstrings
+        docstring_num = 0
         # Find CASTs for each of these files
         cast = cast_dict[file]
         # Determine number of import statements for each file
-        imports = match.findall(cast, match.FunctionDef.get_docstring())
-        imports_dictionary[file] = len(imports)
+        funcdefs = match.findall(cast, match.FunctionDef())
+        # store the number of functions
+        func_count[file] = {"function" : 0, "docstring": 0}
+        func_count[file]["function"] = len(funcdefs)
+        # iterate and count the number of docstrings
+        for node in funcdefs:
+            if node.get_docstring():
+                docstring_num += 1
+        func_count[file]["docstring"] = docstring_num
 
-    return imports_dictionary
+    return func_count
+
+def get_missing_docstrings(func_count: Dict) -> int:
+    """Find the number of functions missing a docstring.
+
+    Args:
+        func_count (Dict): function and docstring counts per file
+
+    Returns:
+        int: total number of functions - total number of docstrings
+    """
+    func_total = 0
+    docstring_total = 0
+    for file_count in func_count.values():
+        func_total += file_count["function"]
+        docstring_total += file_count["docstring"]
+    return func_total - docstring_total
 
 
 if __name__ == "__main__":
@@ -29,5 +54,5 @@ if __name__ == "__main__":
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
 
-    imports_dictionary = match_imports(tree_dict)
-    print(imports_dictionary)
+    funcdefs_dictionary = match_imports(tree_dict)
+    print(get_missing_docstrings(funcdefs_dictionary))
