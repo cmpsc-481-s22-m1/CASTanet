428f16e454085d2cfaf9e0ab7e119bd005acd9ae
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-25 10:38:35 -0500

    Fixed linting errors

--- tests/if_tests.py
+++ tests/if_tests.py
@@ -1,6 +1,7 @@
 """This module test the counter.py module regarding if statements"""
 from castanet import counter
 from castanet import generate_trees as generator
+import libcst.matchers as match
 
 def test_identifing_ifs():
     """Check that match_ifstatements identifies all of the if-statements in a directory"""
@@ -9,4 +10,4 @@ def test_identifing_ifs():
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
     if_dictionary = counter.match_imports(tree_dict)
-    assert if_dictionary == {'say_hello.py': 1, 'init.py': 0}
+    assert if_dictionary == {'say_hello.py': 1, '__init__.py': 0}
