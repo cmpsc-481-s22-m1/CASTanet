ffe4f0b22997aa4c780499be9651d7b850ee3e51
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-25 10:40:36 -0500

    Fixed function name error

--- tests/if_tests.py
+++ tests/if_tests.py
@@ -1,7 +1,6 @@
 """This module test the counter.py module regarding if statements"""
 from castanet import counter
 from castanet import generate_trees as generator
-import libcst.matchers as match
 
 def test_identifing_ifs():
     """Check that match_ifstatements identifies all of the if-statements in a directory"""
@@ -9,5 +8,5 @@ def test_identifing_ifs():
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
-    if_dictionary = counter.match_imports(tree_dict)
+    if_dictionary = counter.match_ifstatements(tree_dict)
     assert if_dictionary == {'say_hello.py': 1, '__init__.py': 0}
