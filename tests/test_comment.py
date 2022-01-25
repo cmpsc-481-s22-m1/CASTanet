04fb7612f9e05b30d3569f41339b09b6aa26dbb8
Baileymatrascia <matrasciab@allegheny.edu>
2022-01-25 10:41:47 -0500

    comment test

---
+++ tests/test_comment.py
@@ -0,0 +1,13 @@
+"""This module test the counter.py module regarding if statements"""
+import pytest
+from castanet import counter
+from castanet import generate_trees as generator
+
+def test_Comment():
+    """Check that match_Comment identifies all of the comments in a directory"""
+    directory = "./hello"
+    file_list = generator.find_python_files(directory)
+    string_file_list = generator.read_files(directory, file_list)
+    tree_dict = generator.generate_cast(string_file_list)
+    Comment_dictionary = counter.match_Comment(tree_dict)
+    assert Comment_dictionary == {'say_hello.py': 1, 'init.py': 0}
