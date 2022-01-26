85f99e9b16e101682ab1bbad527b2066f7280aaf
Baileymatrascia <matrasciab@allegheny.edu>
2022-01-26 12:32:13 -0500

    changes

--- castanet/counter.py
+++ castanet/counter.py
@@ -1,14 +1,6 @@
-# Import necessary types for typing
-from typing import List, Tuple, Dict, Optional
-# Import LibCST
-import libcst as cst
-import generate_trees as generator
+"""This module counts instances in Python files."""
 
 import libcst.matchers as match
-
-# TODO: Create function definitions related to all necessary user stories (should look somewhat like match_imports)
-# TODO: Modify annotations to have node key as type and key as necessary information
-
 
 def match_imports(cast_dict):
     """A function for counting the number of if statements in a Python program."""
@@ -18,13 +10,23 @@ def match_imports(cast_dict):
         # Find CASTs for each of these files
         cast = cast_dict[file]
         # Determine number of import statements for each file
-        imports = match.findall(cast, match.Import())
-        imports_dictionary[file] = len(imports)
+        imports_list = match.findall(cast, match.Import())
+        imports_dictionary[file] = len(imports_list)
 
     return imports_dictionary
 
-    #matcher function to iterate through a file to get the total number of comments in a file
-def match_Comment(cast_dict):
+
+def total_imports(imports_dict):
+    """Find and combine the number of import statements in Python files in a specific directory."""
+    total = 0
+    for file in imports_dict:
+        amount_of_imports = imports_dict[file]
+        total += amount_of_imports
+
+    return total
+
+#matcher function to iterate through a file to get the total number of comments in a file
+def match_comment(cast_dict):
     """A function for counting the number of comments statements in a Python program."""
     comment_dictionary = {}
     # Iterate through all of the Python files in a directory
@@ -32,29 +34,17 @@ def match_Comment(cast_dict):
         # Find CASTs for each of these files
         cast = cast_dict[file]
         # Determine number of comments for each file
-        Comment = match.findall(cast, match.Comment())
-        comment_dictionary[file] = len(Comment)
+        comment = match.findall(cast, match.Comment())
+        comment_dictionary[file] = len(comment)
 
     return comment_dictionary
 
-#iterates through the number of comments found in each file and adds them up to get the total number of comments
+#iterates through the number of comments found in each file
+#and adds them up to get the total number of comments
 def total_comment(cast_dict):
+    """ function to add the total number of comments in dir """
     count = 0
     for file in cast_dict:
         comments = cast_dict[file]
         count += comments
     return count
-
-
-if __name__ == "__main__":
-    directory = "/home/mkapfhammer/Documents/Allegheny/2022/Spring/CMPSC481/project-team-1/hello"
-    file_list = generator.find_python_files(directory)
-    string_file_list = generator.read_files(directory, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
-
-    imports_dictionary = match_imports(tree_dict)
-    print(imports_dictionary)
-
-    comment_dictionary = match_Comment(tree_dict)
-    number_of_comments = total_comment(Comment_dictionary)
-    print(Comment_dictionary)
--- tests/test_comment.py
+++ tests/test_comment.py
@@ -2,11 +2,17 @@
 from castanet import counter
 from castanet import generate_trees as generator
 
-def test_comment():
+def test_match_comment_returns_correct_number_comments():
     """Check that match_Comment identifies all of the comments in a directory"""
     directory = "./hello"
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
-    comment_dictionary = counter.match_Comment(tree_dict)
-    assert comment_dictionary == {'say_hello.py': 1, 'init.py': 0}
+    comment_dictionary = counter.match_comment(tree_dict)
+    assert comment_dictionary == {'say_hello.py': 1, '__init__.py': 0}
+
+def test_total_comment_returns_correct_number_comments():
+    """Checks that total_comment adds all identified comments"""
+    comment_dictionaries = {'say_hello.py': 1, '__init__.py': 3}
+    number_comments = counter.total_comment(comment_dictionaries)
+    assert number_comments == 4
