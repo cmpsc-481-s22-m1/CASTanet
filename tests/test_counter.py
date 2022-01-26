b4b191b556ed706bd2c1de25f665264695559b9b
hinckleyc <46756003+hinckleyc@users.noreply.github.com>
2022-01-26 00:02:33 -0500

    Added test cases

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -4,7 +4,7 @@ from castanet import counter
 from castanet import generate_trees as generator
 
 
-def test_countloops():
+def test_countloops1():
     """Check that functions and docstrings are counted correctly"""
     directory = "./hello"
     file_list = generator.find_python_files(directory)
@@ -17,11 +17,20 @@ def test_countloops():
     assert imports_dictionary == ({'__init__.py': 0, 'test.py': 0, 'say_hello.py': 1,
                                   'test-while.py': 0})
    
-def test_stacks():
-    """Check that functions and docstrings are counted correctly"""
-    directory = "./hello"
+def test_countloops2():
+    """Uses count_forloops to identify all the if statements in the test_files directory"""
+    directory = "./test_files"
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
-    stack = counter.couunt_forloops(tree_dict)
-    assert stack == {'test.py': 1}
+    if_dictionary = counter.count_forloops(tree_dict)
+    assert if_dictionary == {'loops.py': 3}
+
+def test_countloops3():
+    """Uses count_whileloops to identify all the if statements in the test_files directory"""
+    directory = "./test_files"
+    file_list = generator.find_python_files(directory)
+    string_file_list = generator.read_files(directory, file_list)
+    tree_dict = generator.generate_cast(string_file_list)
+    if_dictionary = counter.count_whileloops(tree_dict)
+    assert if_dictionary == {'loops.py': 2}
