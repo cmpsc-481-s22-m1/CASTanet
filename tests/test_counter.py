e3320ad024c91692c87c33b5cdcf786790071647
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2022-01-26 10:08:14 -0500

    Modify tests as necessary.

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -5,19 +5,19 @@ from castanet import counter
 
 def test_import_dictionary():
     """Test that the dictionary for imports is being generated correctly."""
-    directory_path = "tests/files_for_tests"
+    directory_path = "./test_files"
     file_list = generator.find_python_files(directory_path)
     string_file_list = generator.read_files(directory_path, file_list)
     tree_dict = generator.generate_cast(string_file_list)
 
     imports_dictionary = counter.match_imports(tree_dict)
 
-    assert len(imports_dictionary) == 5
+    assert len(imports_dictionary) == 2
 
 
 def test_calculate_total_imports():
     """Test that imports are totalled for each file in a directory."""
-    directory_path = "tests/files_for_tests"
+    directory_path = "./test_files"
     file_list = generator.find_python_files(directory_path)
     string_file_list = generator.read_files(directory_path, file_list)
     tree_dict = generator.generate_cast(string_file_list)
