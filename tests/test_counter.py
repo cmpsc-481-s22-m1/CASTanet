402a0ff248771cd18240c628b0589639e5bc3668
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2022-01-26 09:45:31 -0500

    Correctly format docstring.

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -4,7 +4,7 @@ from castanet import generate_trees as generator
 from castanet import counter
 
 def test_import_dictionary():
-    """Test that the dictionary for imports is being generated correctly"""
+    """Test that the dictionary for imports is being generated correctly."""
     directory_path = "tests/files_for_tests"
     file_list = generator.find_python_files(directory_path)
     string_file_list = generator.read_files(directory_path, file_list)
