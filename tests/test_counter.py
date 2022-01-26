4e22a2cf0ab2894d2c6f23b115c6cfe7564080f0
hinckleyc <46756003+hinckleyc@users.noreply.github.com>
2022-01-25 23:49:51 -0500

    editing lint errors

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -17,11 +17,11 @@ def test_countloops():
     assert imports_dictionary == ({'__init__.py': 0, 'test.py': 0, 'say_hello.py': 1,
                                   'test-while.py': 0})
     
-def test_countloops():
+def test_stacks():
     """Check that functions and docstrings are counted correctly"""
     directory = "./hello"
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
-    stack = counter.match_stack_statements(tree_dict)
+    stack = counter.match_forloops(tree_dict)
     assert stack == {'test.py': 1}
