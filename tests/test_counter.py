ba09dcc43380f29b2e9c895d39a24fcb3809536e
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-26 09:56:11 -0500

    Fixed Test Cases

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -5,21 +5,21 @@ from castanet import generate_trees as generator
 def test_match_if_statements_1():
     """Uses match_if_statements to identify all the if statements in the test_files directory"""
     directory = "./test_files"
-    if_dictionary = {}
+    if_list = []
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
     ifs = counter.match_if_statements(tree_dict)
-    if_dictionary += ifs
-    assert len(if_dictionary) == 5
+    if_list += ifs
+    assert len(if_list) == 5
 
 def test_match_if_statements_2():
     """Uses match_if_statements to identify all the if-statements in the hello directory"""
     directory = "./hello"
-    if_dictionary = {}
+    if_list = []
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
     ifs = counter.match_if_statements(tree_dict)
-    if_dictionary += ifs
-    assert len(if_dictionary) == 2
+    if_list += ifs
+    assert len(if_list) == 2
