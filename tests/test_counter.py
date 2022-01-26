ff531bdce32ba93f28bc7f97e3d4a701eb763a71
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-26 10:04:57 -0500

    Test Cases Fix

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -5,21 +5,17 @@ from castanet import generate_trees as generator
 def test_match_if_statements_1():
     """Uses match_if_statements to identify all the if statements in the test_files directory"""
     directory = "./test_files"
-    if_list = []
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
-    ifs = counter.match_if_statements(tree_dict)
-    if_list += ifs
-    assert len(if_list) == 5
+    if_dictionary = counter.match_if_statements(tree_dict)
+    assert if_dictionary == {'blackjack.py': 5}
 
 def test_match_if_statements_2():
     """Uses match_if_statements to identify all the if-statements in the hello directory"""
     directory = "./hello"
-    if_list = []
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
-    ifs = counter.match_if_statements(tree_dict)
-    if_list += ifs
-    assert len(if_list) == 2
+    if_dictionary = counter.match_if_statements(tree_dict)
+    assert if_dictionary == {'say_hello.py': 1, '__init__.py': 0}
