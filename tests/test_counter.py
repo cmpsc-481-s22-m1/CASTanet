ed87617627dcfe44856dda2e0bfcf1f745be31bf
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-25 21:24:27 -0500

    Fixed Linting Error

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,7 +10,7 @@ def test_match_if_statements_1():
     tree_dict = generator.generate_cast(string_file_list)
     if_dictionary = counter.match_if_statements(tree_dict)
     assert if_dictionary == {'blackjack.py': 5}
-    
+
 def test_match_if_statements_2():
     """Uses match_if_statements to identify all the if-statements in the hello directory"""
     directory = "./hello"
