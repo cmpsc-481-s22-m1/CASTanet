2c6523018c60405b0b06f8ae3f380ab16c71aaf0
Baileymatrascia <matrasciab@allegheny.edu>
2022-01-27 10:36:44 -0500

    captialize variables

--- test_files/comments.py
+++ test_files/comments.py
@@ -1,19 +1,19 @@
 """A module for testing checking comment counting in CASTanet."""
 
 #set of values for operations
-num1 = 10
-num2 = 20
-num3 = 25
-num4 = 30
+NUM1 = 10
+NUM2 = 20
+NUM3 = 25
+NUM4 = 30
 
 #addition opperation to add values from set
-sum1 = num2 + num3
+SUM1 = NUM2 + NUM3
 
 #subtraction opperation to subtract values from set
-sum2 = num4 - num1
+SUM2 = NUM4 - NUM1
 
 #print value for additon opperation
-print("the sum of 20 and 25 is:", sum1)
+print("the sum of 20 and 25 is:", SUM1)
 
 #print value for subtraction opperation
-print("the difference of 30 and 10 is:", sum2)
+print("the difference of 30 and 10 is:", SUM2)
