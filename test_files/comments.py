85e97f8adf62de02502a6fd378c895eb94fad49b
Baileymatrascia <matrasciab@allegheny.edu>
2022-01-27 10:28:38 -0500

    add code with more comments to improve test for comments

--- test_files/comments.py
+++ test_files/comments.py
@@ -1,1 +1,19 @@
 """A module for testing checking comment counting in CASTanet."""
+
+#set of values for operations
+num1 = 10
+num2 = 20
+num3 = 25
+num4 = 30
+
+#addition opperation to add values from set
+sum1 = num2 + num3
+
+#subtraction opperation to subtract values from set
+sum2 = num4 - num1
+
+#print value for additon opperation
+print("the sum of 20 and 25 is:", sum1)
+
+#print value for subtraction opperation
+print("the difference of 30 and 10 is:", sum2)
