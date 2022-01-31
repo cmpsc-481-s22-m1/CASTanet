d5c1d58bc573b035253b0985db0b2bb6ba34dd2a
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-31 10:08:59 -0500

    Added class definition and docstring functionality

--- castanet/counter.py
+++ castanet/counter.py
@@ -10,26 +10,43 @@ import libcst.matchers as match
 # TODO: Modify annotations to have node key as type and key as necessary information
 
 
-def match_imports(cast_dict):
-    """A function for counting the number of if statements in a Python program."""
-    imports_dictionary = {}
+def match_class_defs(cast_dict):
+    """A function for counting the number of class definitions in a Python program."""
+    class_defs_dictionary = {}
     # Iterate through all of the Python files in a directory
     for file in cast_dict:
+        docstring_num = 0
         # Find CASTs for each of these files
         cast = cast_dict[file]
-        # Determine number of import statements for each file
-        imports = match.findall(cast, match.Import())
-        imports_dictionary[file] = len(imports)
+        # Determine number of class definitions for each file
+        class_defs = match.findall(cast, match.ClassDef())
+        class_defs_dictionary[file] = {"class" : 0, "docstring" : 0}
+        class_defs_dictionary[file]["class"] = len(class_defs)
 
-    return imports_dictionary
+        for node in class_defs:
+            if node.get_docstring():
+                docstring_num += 1
+        class_defs_dictionary[file]["docstring"] = docstring_num
 
+    return class_defs_dictionary
+
+def count_class_defs_without_docstrings(class_defs_dictionary: Dict) -> int:
+    """Find the number of class definitions missing a docstring"""
+    class_total = 0
+    docstring_total = 0
+    for file_count in class_defs_dictionary.values():
+        class_total += file_count["class"]
+        docstring_total += file_count["docstring"]
+    return class_total - docstring_total
 
 if __name__ == "__main__":
-    directory = "/home/mkapfhammer/Documents/Allegheny/2022/Spring/CMPSC481/project-team-1/hello"
+    directory = "/Users/nolanthompson/Desktop/College/CS481/CASTanet/hello"
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
 
-    imports_dictionary = match_imports(tree_dict)
-    print(imports_dictionary)
+    class_def_dictionary = match_class_defs(tree_dict)
+    class_def_without_docstring = count_class_defs_without_docstrings(class_def_dictionary)
+    print(class_def_dictionary)
+    print(class_def_without_docstring)
 
