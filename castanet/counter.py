0938a2d149056f70fe876c01467eac8ae3cafffd
Nolan Thompson <thompsonn2@allegheny.edu>
2022-01-31 10:27:37 -0500

    Added implementation for class defs

--- castanet/counter.py
+++ castanet/counter.py
@@ -1,121 +1,52 @@
-"""This module counts instances of statements in Python files."""
-from typing import Dict
+# Import necessary types for typing
+from typing import List, Tuple, Dict, Optional
+# Import LibCST
+import libcst as cst
+import generate_trees as generator
+
 import libcst.matchers as match
+    
+# TODO: Create function definitions related to all necessary user stories (should look somewhat like match_imports)
+# TODO: Modify annotations to have node key as type and key as necessary information
 
-def match_imports(cast_dict):
-    """A function for counting the number of import statements in a Python program."""
-    imports_dictionary = {}
+
+def match_class_defs(cast_dict):
+    """A function for counting the number of class definitions in a Python program."""
+    class_defs_dictionary = {}
     # Iterate through all of the Python files in a directory
     for file in cast_dict:
-        # Find CASTs for each of these files
-        cast = cast_dict[file]
-        # Determine number of import statements for each file
-        imports_list = match.findall(cast, match.Import())
-        imports_dictionary[file] = len(imports_list)
-
-    return imports_dictionary
-
-
-def match_comment(cast_dict):
-    """A function for counting the number of comments statements in a Python program."""
-    comment_dictionary = {}
-    for file in cast_dict:
-        # Find CASTs for each of these files
-        cast = cast_dict[file]
-        # Determine number of comments for each file
-        comment = match.findall(cast, match.Comment())
-        comment_dictionary[file] = len(comment)
-
-    return comment_dictionary
-
-
-def count_whileloops(cast_dict):
-    """A function for counting the number of while loops in a Python program."""
-    while_dictionary = {}
-    # Iterate through all of the Python files in a directory
-    for file in cast_dict:
-        # Find CASTs for each of these files
-        cast = cast_dict[file]
-        while_loops = match.findall(cast, match.While())
-        while_dictionary[file] = len(while_loops)
-
-    return while_dictionary
-
-
-def count_forloops(cast_dict):
-    """A function for counting the number of while loops in a Python program."""
-    for_dictionary = {}
-    # Iterate through all of the Python files in a directory
-    for file in cast_dict:
-        # Find CASTs for each of these files
-        cast = cast_dict[file]
-        # Determine number of for statements for each file
-        imports = match.findall(cast, match.For())
-        for_dictionary[file] = len(imports)
-
-    return for_dictionary
-
-
-def match_if_statements(cast_dict):
-    """A function for counting the number of if statements in a Python program."""
-    if_statements_dictionary = {}
-    # Iterate through all of the Python files in a directory
-    for file in cast_dict:
-        # Find CASTs for each of these files
-        cast = cast_dict[file]
-        # Determine number of comments for each file
-        ifs = match.findall(cast, match.If())
-        if_statements_dictionary[file] = len(ifs)
-
-    return if_statements_dictionary
-
-
-def match_funcdefs(cast_dict):
-    """A function for counting the number of function definitions in a Python program."""
-    func_count = {}
-    # Iterate through all of the Python files in a directory
-    for file in cast_dict:
-        # track the number of docstrings
         docstring_num = 0
         # Find CASTs for each of these files
         cast = cast_dict[file]
-        # Determine number of import statements for each file
-        funcdefs = match.findall(cast, match.FunctionDef())
-        # store the number of functions
-        func_count[file] = {"function" : 0, "docstring": 0}
-        func_count[file]["function"] = len(funcdefs)
-        # iterate and count the number of docstrings
-        for node in funcdefs:
+        # Determine number of class definitions for each file
+        class_defs = match.findall(cast, match.ClassDef())
+        class_defs_dictionary[file] = {"class" : 0, "docstring" : 0}
+        class_defs_dictionary[file]["class"] = len(class_defs)
+
+        for node in class_defs:
             if node.get_docstring():
                 docstring_num += 1
-        func_count[file]["docstring"] = docstring_num
+        class_defs_dictionary[file]["docstring"] = docstring_num
 
-    return func_count
+    return class_defs_dictionary
 
-
-def count_function_without_docstrings(func_count: Dict) -> int:
-    """Find the number of functions missing a docstring.
-
-    Args:
-        func_count (Dict): function and docstring counts per file
-
-    Returns:
-        int: total number of functions - total number of docstrings
-    """
-    func_total = 0
+def count_class_defs_without_docstrings(class_defs_dictionary: Dict) -> int:
+    """Find the number of class definitions missing a docstring"""
+    class_total = 0
     docstring_total = 0
-    for file_count in func_count.values():
-        func_total += file_count["function"]
+    for file_count in class_defs_dictionary.values():
+        class_total += file_count["class"]
         docstring_total += file_count["docstring"]
-    return func_total - docstring_total
+    return class_total - docstring_total
 
+if __name__ == "__main__":
+    directory = "/Users/nolanthompson/Desktop/College/CS481/CASTanet/hello"
+    file_list = generator.find_python_files(directory)
+    string_file_list = generator.read_files(directory, file_list)
+    tree_dict = generator.generate_cast(string_file_list)
 
-def sum_cast_dict(cast_dict):
-    """A function for calculating the sums of values from dictionaries in previous functions."""
-    total = 0
-    # Total imports
-    for file in cast_dict:
-        amount = cast_dict[file]
-        total += amount
+    class_def_dictionary = match_class_defs(tree_dict)
+    class_def_without_docstring = count_class_defs_without_docstrings(class_def_dictionary)
+    print(class_def_dictionary)
+    print(class_def_without_docstring)
 
-    return total
