d9df90c883e26e3a06c5394523b4ca5f77391288
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2022-02-02 10:07:27 -0500

    Reconfigure tests to use refactored function for generating CASTs.

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -4,12 +4,18 @@ from castanet import counter
 from castanet import generate_trees as generator
 
 
-def test_for_loops_dict():
-    """Check that for loops are counted correctly."""
+def create_casts():
     directory = "./test_files"
     file_list = generator.find_python_files(directory)
     string_file_list = generator.read_files(directory, file_list)
     tree_dict = generator.generate_cast(string_file_list)
+    return tree_dict
+
+
+def test_for_loops_dict():
+    """Check that for loops are counted correctly."""
+    tree_dict = create_casts()
+
     for_dictionary = counter.count_forloops(tree_dict)
     amount_for_loops =  counter.sum_cast_dict(for_dictionary)
 
@@ -19,10 +25,7 @@ def test_for_loops_dict():
 
 def test_count_while_loops():
     """Check that while loops are counted correctly."""
-    directory = "./test_files"
-    file_list = generator.find_python_files(directory)
-    string_file_list = generator.read_files(directory, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
     while_dictionary = counter.count_whileloops(tree_dict)
     amount_while_loops = counter.sum_cast_dict(while_dictionary)
 
@@ -33,30 +36,21 @@ def test_count_while_loops():
 
 def test_import_dictionary():
     """Test that the dictionary for imports is being generated correctly."""
-    directory_path = "./test_files"
-    file_list = generator.find_python_files(directory_path)
-    string_file_list = generator.read_files(directory_path, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
     if_dictionary = counter.match_if_statements(tree_dict)
     assert len(if_dictionary) == 5
 
 
 def test_match_if_statements_2():
     """Uses match_if_statements to identify all the if-statements in the test_files directory."""
-    directory_path = "./test_files"
-    file_list = generator.find_python_files(directory_path)
-    string_file_list = generator.read_files(directory_path, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
     if_dictionary = counter.match_if_statements(tree_dict)
     assert len(if_dictionary) == 5
 
 
 def test_calculate_total_imports():
     """Test that imports are totalled for each file in a directory."""
-    directory_path = "./test_files"
-    file_list = generator.find_python_files(directory_path)
-    string_file_list = generator.read_files(directory_path, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
 
     imports_dictionary = counter.match_imports(tree_dict)
 
@@ -67,10 +61,7 @@ def test_calculate_total_imports():
 
 def test_match_imports_len():
     """Check that the imports are counted correctly."""
-    directory = "./test_files"
-    file_list = generator.find_python_files(directory)
-    string_file_list = generator.read_files(directory, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
     imports_dictionary = counter.match_imports(tree_dict)
     # assert imports_dictionary == {'funcdefs_test_file.py': 0, '__init__.py': 0}
     assert len(imports_dictionary) == 5
@@ -78,10 +69,7 @@ def test_match_imports_len():
 
 def test_funcdef_docstring_count():
     """Check that functions and docstrings are counted correctly."""
-    directory = "./test_files"
-    file_list = generator.find_python_files(directory)
-    string_file_list = generator.read_files(directory, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
     funcdefs_dictionary = counter.match_funcdefs(tree_dict)
     # assert funcdefs_dictionary == {'funcdefs_test_file.py': {'function': 3, 'docstring': 3},
     #  '__init__.py': {'function': 0, 'docstring': 0}}
@@ -90,10 +78,7 @@ def test_funcdef_docstring_count():
 
 def test_match_comment_returns_correct_number_comments():
     """Check that match_Comment identifies all of the comments in a directory."""
-    directory = "./test_files"
-    file_list = generator.find_python_files(directory)
-    string_file_list = generator.read_files(directory, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
     comment_dictionary = counter.match_comment(tree_dict)
     assert len(comment_dictionary) == 5
 
@@ -106,10 +91,7 @@ def test_total_comment_returns_correct_number_comments():
 
 def test_count_function_arguments():
     """Check that CASTanet returns the correct number of arguments for a given function."""
-    directory = "./test_files"
-    file_list = generator.find_python_files(directory)
-    string_file_list = generator.read_files(directory, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
 
     function_arguments = counter.count_function_arguments(tree_dict, "greet")
 
@@ -117,10 +99,7 @@ def test_count_function_arguments():
 
 def test_non_existing_function():
     """Check that CASTanet returns an error when a function is not found."""
-    directory = "./test_files"
-    file_list = generator.find_python_files(directory)
-    string_file_list = generator.read_files(directory, file_list)
-    tree_dict = generator.generate_cast(string_file_list)
+    tree_dict = create_casts()
 
     function_arguments = counter.count_function_arguments(tree_dict, "unknown")
 
