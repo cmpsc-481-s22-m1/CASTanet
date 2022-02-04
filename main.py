275d428f178c5e26fc35a0baea60896aee7bdf30
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2022-02-04 14:58:16 -0500

    Correct linting and testing errors.

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def number_functions_in_module(directory_path:str, directory_or_file: str):
 def if_statements(directory_path):
     """Determine number of if statements in a Python directory."""
     cast_dict = generate_trees(directory_path)
-    if_dictionary = counter.match_if_statements(cast_dict)
+    if_dictionary = counter.count_if_statements(cast_dict)
     total_if_statements = counter.sum_dict_vals(if_dictionary)
     print("Number of if statements: ", str(total_if_statements))
 
@@ -46,8 +46,8 @@ def if_statements(directory_path):
 def looping_constructs(directory_path: str):
     """Determine number of looping constructs."""
     cast_dict = generate_trees(directory_path)
-    while_loops_dict = counter.count_whileloops(cast_dict)
-    for_loops_dict = counter.count_forloops(cast_dict)
+    while_loops_dict = counter.count_while_loops(cast_dict)
+    for_loops_dict = counter.count_for_loops(cast_dict)
     total_for_loops = counter.sum_dict_vals(for_loops_dict)
     total_while_loops = counter.sum_dict_vals(while_loops_dict)
     total_loops = total_for_loops + total_while_loops
@@ -74,7 +74,7 @@ def assignment(directory_path: str):
 def comments(directory_path: str):
     """Determine number of comments."""
     cast_dict = generate_trees(directory_path)
-    comment_dictionary = counter.match_comment(cast_dict)
+    comment_dictionary = counter.count_comment(cast_dict)
     total_comments = counter.sum_dict_vals(comment_dictionary)
     print("Number of comments: " + str(total_comments))
 
@@ -83,7 +83,7 @@ def comments(directory_path: str):
 def total_functions(directory_path: str):
     """Determine number of functions in a directory."""
     cast_dict = generate_trees(directory_path)
-    functions_dictionary = counter.match_class_defs(cast_dict)
+    functions_dictionary = counter.count_class_defs(cast_dict)
     number_of_functions = counter.sum_dict_vals(functions_dictionary)
 
     print("Number of functions: " + str(number_of_functions))
@@ -104,7 +104,7 @@ def functions_without_docstrings(directory_path: str):
 def imports(directory_path: str):
     """Determine number of import statements."""
     cast_dict = generate_trees(directory_path)
-    import_dictionary = counter.match_imports(cast_dict)
+    import_dictionary = counter.count_imports(cast_dict)
     total_imports = counter.sum_dict_vals(import_dictionary)
     print("Number of imports: " + str(total_imports))
 
@@ -113,7 +113,7 @@ def imports(directory_path: str):
 def total_classes(directory_path: str):
     """Determine number of classes in a directory."""
     cast_dict = generate_trees(directory_path)
-    classes_dictionary = counter.match_class_defs(cast_dict)
+    classes_dictionary = counter.count_class_defs(cast_dict)
     number_of_classes = counter.sum_dict_vals(classes_dictionary)
     print("Number of classes: " + str(number_of_classes))
 
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -19,7 +19,7 @@ def test_count_for_loops():
     tree_dict = create_casts()
 
     for_dictionary = counter.count_for_loops(tree_dict)
-    amount_for_loops =  counter.sum_cast_dict(for_dictionary)
+    amount_for_loops =  counter.sum_dict_vals(for_dictionary)
 
     correct_dictionary = {'__init__.py': 0, 'classdefs.py': 0, 'comments.py': 0,
         'funcdefs_test_file.py': 0, 'if_statements.py': 0, 'loops.py': 3}
@@ -32,7 +32,7 @@ def test_count_while_loops():
     """Test that while loops are counted correctly."""
     tree_dict = create_casts()
     while_dictionary = counter.count_while_loops(tree_dict)
-    amount_while_loops = counter.sum_cast_dict(while_dictionary)
+    amount_while_loops = counter.sum_dict_vals(while_dictionary)
 
     correct_dictionary = {'__init__.py': 0, 'classdefs.py': 0, 'comments.py': 0,
         'funcdefs_test_file.py': 0, 'if_statements.py': 0, 'loops.py': 2}
@@ -45,7 +45,7 @@ def test_count_imports():
     """Test that import statements are counted correctly."""
     tree_dict = create_casts()
     import_dictionary = counter.count_imports(tree_dict)
-    amount_imports = counter.sum_cast_dict(import_dictionary)
+    amount_imports = counter.sum_dict_vals(import_dictionary)
 
     correct_dictionary = {'__init__.py': 0, 'classdefs.py': 0, 'comments.py': 0,
         'funcdefs_test_file.py': 1, 'if_statements.py': 0, 'loops.py': 0}
@@ -58,7 +58,7 @@ def test_count_if_statements():
     """Test that if statements are counted correctly."""
     tree_dict = create_casts()
     if_dictionary = counter.count_if_statements(tree_dict)
-    amount_ifs = counter.sum_cast_dict(if_dictionary)
+    amount_ifs = counter.sum_dict_vals(if_dictionary)
 
     correct_dictionary = {'__init__.py': 0, 'classdefs.py': 0, 'comments.py': 0,
         'funcdefs_test_file.py': 1, 'if_statements.py': 5, 'loops.py': 0}
@@ -79,7 +79,7 @@ def test_count_comments():
     """Check that match_Comment identifies all of the comments in a directory."""
     tree_dict = create_casts()
     comment_dictionary = counter.count_comment(tree_dict)
-    amount_comments = counter.sum_cast_dict(comment_dictionary)
+    amount_comments = counter.sum_dict_vals(comment_dictionary)
 
     correct_dictionary = {'__init__.py': 0, 'classdefs.py': 0,'comments.py': 5,
         'funcdefs_test_file.py': 0, 'if_statements.py': 3, 'loops.py': 0}
